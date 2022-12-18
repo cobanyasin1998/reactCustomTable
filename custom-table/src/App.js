d44697e02a286db434f5156c6cd92b227671ac2a
Yasin <yasincoban1998@outlook.com>
2022-12-19 00:00:45 +0300

    responsive-searchable

--- custom-table/src/App.js
+++ custom-table/src/App.js
@@ -1,10 +1,71 @@
 import Table from "./table";
 
-
 function App() {
+  const users = [
+    {
+      name: "Yasin",
+      surname: "Çoban",
+      email: "cobanyasin@gmail.com",
+      age: 24,
+    },
+    {
+      name: "Meltem",
+      surname: "Çoban",
+      email: "cobanmeltem@gmail.com",
+      age: 25,
+    },
+    {
+      name: "Ayça",
+      surname: "Karabulut",
+      email: "karabulutayca@gmail.com",
+      age: 25,
+    },
+    {
+      name: "Mesut",
+      surname: "Karabulut",
+      email: "karabulutcagri@gmail.com",
+      age: 26,
+    },
+  ];
   return (
     <div className="p-4">
-      <Table/>
+      <Table
+        searchable={true}
+        head={[
+          {
+            name: "Ad-Soyad",
+          },
+          {
+            name: "E-Posta",
+          },
+          {
+            name: "Yaş",
+          },
+          {
+            name: "İşlemler",
+            width:"200"
+          },
+        ]}
+        body={users.map((user) => [
+          user.name,
+          user.email,
+          user.age,
+          [
+            <button
+              className="h-8 px-4 flex items-center rounded bg-blue-400 text-white"
+              disabled
+            >
+              Düzenle
+            </button>,
+            <button
+              className="h-8 px-4 flex items-center rounded bg-red-400 text-white"
+              disabled
+            >
+              Sil
+            </button>,
+          ],
+        ])}
+      />
     </div>
   );
 }
--- custom-table/src/table.js
+++ custom-table/src/table.js
@@ -1,15 +1,63 @@
-export default function Table() {
+import { useState } from "react";
+
+export default function Table({ head, body, searchable }) {
+  const [search, setSearch] = useState("");
+  const filteredData = body.filter((items) =>
+    items.some((item) =>
+      item
+        .toString()
+        .toLocaleLowerCase("TR")
+        .includes(search.toLocaleLowerCase("TR"))
+    )
+  );
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>Başlık 1</th>
-            <th>Başlık 2</th>
-            <th>Başlık 3</th>
-          </tr>
-        </thead>
-      </table>
+      {searchable && (
+        <div className="mb-4">
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="Tabloda Ara"
+            className="h-10 border rounded text-sm px-4 w-full border-gray-200 outline-none focus:border-black-50"
+          ></input>
+        </div>
+      )}
+      <div className="w-full border rounded p-4">
+        <table className="w-full">
+          <thead>
+            <tr>
+              {head.map((h, key) => (
+                <th
+                  width={h?.width}
+                  className="text-left bg-gray-50 text-sm font-semibold text-gray-500 p-3 border-b"
+                  key={key}
+                >
+                  {h.name}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((items, key) => (
+              <tr className="group" key={key}>
+                {items.map((item, keyItem) => (
+                  <td
+                    className="p-3 text-sm group-hover:bg-blue-100"
+                    key={keyItem}
+                  >
+                    {Array.isArray(item) ? (
+                      <div className="flex gap-x-2.5">{item}</div>
+                    ) : (
+                      item
+                    )}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
